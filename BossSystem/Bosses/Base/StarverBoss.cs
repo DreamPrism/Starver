f104cb3ff3092a00dc6aff197f31a114f651ffd5
GUZZ07 <50436210+GUZZ07@users.noreply.github.com>
2019-07-14 14:29:19 +0800

    Add files via upload

---
+++ BossSystem/Bosses/Base/StarverBoss.cs
@@ -0,0 +1,522 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using TerrariaApi.Server;
+using TShockAPI;
+using Terraria.Localization;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security;
+namespace Starvers.BossSystem.Bosses.Base
+{
+	using Vector = TOFOUT.Terraria.Server.Vector2;
+	using BigInt = System.Numerics.BigInteger;
+	public abstract partial class StarverBoss : StarverEntity
+	{
+		#region Properties
+		public int Life { get { return RealNPC.life; } set { RealNPC.life = value; } }
+		public int LifeMax { get { return RealNPC.lifeMax; } set { RealNPC.lifeMax = value; } }
+		public int Defense { get { return RealNPC.defense; } set { RealNPC.defense = value; } }
+		public int RawType { get; protected set; } = NPCID.BlueSlime;
+		public float MaxDistance { get; protected set; } = 500f;
+		public bool IgnoreDistance { get; protected set; } = true;
+		public bool Downed { get; protected set; } 
+		/// <summary>
+		/// 召唤时无提示
+		/// </summary>
+		public bool Silence { get; protected set; } 
+		public string Name { get; protected set; }
+		public Vector2 LastCenter { get; protected set; }
+		/// <summary>
+		/// boss伤害系数
+		/// </summary>
+		public override float DamageIndex
+		{
+			get
+			{
+				return (float)(Math.Sqrt(LifesMax / (Lifes + 1)) + (float)Level / Criticallevel);
+			}
+		}
+		/// <summary>
+		/// boss当前状态
+		/// </summary>
+		protected BossMode Mode
+		{
+			get
+			{
+				return mode;
+			}
+			set
+			{
+				mode = value;
+				LastCenter = Center;
+#if DEBUG
+				TSPlayer.All.SendInfoMessage("{0}的模式由{1}更改为{2}", Name, lastMode, mode);
+#endif
+			}
+		}
+		/// <summary>
+		/// 相对位置(要加上玩家坐标)
+		/// </summary>
+		protected Vector2 RelativePos
+		{
+			get
+			{
+				return Center - TargetPlayer.Center;
+			}
+			set
+			{
+				Center = TargetPlayer.Center + value;
+			}
+		}
+		#endregion
+		#region Fields
+		protected DropItem[] Drops;
+		protected bool NightBoss = true;
+		protected bool ExVersion;
+		protected string DisplayName;
+		protected string FullName;
+		protected int Lifes;
+		protected int LifesMax;
+		protected int DefaultLifes;
+		protected int DefaultLife;
+		protected int DefaultDefense = 10;
+		protected unsafe float* StarverAI;
+		protected uint modetime;
+		protected byte LifeperPlayerType = ByLife;
+		protected Vector vector = new Vector(10, 10);
+		protected Vector WhereToGo;
+		protected BossMode lastMode;
+		private int[] Walls;
+		private int TimeSpan;
+		private bool WallStop;
+		private StarverPlayer WallTarget;
+		private Vector[] WallVector;
+		private Vector StopPos;
+		private bool WallActive;
+		private BossMode mode;
+		private readonly int NumOfAIs;
+		private const int MaxWalls = 30;
+		private const int WallDamage = 2250;
+		#endregion
+		#region ctor
+		private StarverBoss()
+		{
+			Name = GetType().Name;
+		}
+		protected unsafe StarverBoss(int ainum):this()
+		{
+			NumOfAIs = ainum;
+			StarverAI = (float*)Marshal.AllocHGlobal(sizeof(float) * NumOfAIs).ToPointer();
+			Level = Criticallevel;
+			DefaultLife = 40000;
+			DefaultDefense = 60;
+			DefaultLifes = 90;
+		}
+		#endregion
+		#region dtor
+		unsafe ~StarverBoss()
+		{
+			if (NumOfAIs > 0)
+			{
+				Marshal.FreeHGlobal(new IntPtr(StarverAI));
+			}
+		}
+		#endregion
+		#region KillMe
+		public override void KillMe()
+		{
+			if(!_active)
+			{
+				return;
+			}
+			AliveBoss--;
+			_active = false;
+			Lifes = 0;
+			base.KillMe();
+			Index = -1;
+		}
+		#endregion
+		#region BeDown
+		protected virtual void BeDown()
+		{
+			Downed = true;
+			_active = false;
+			ExpGive = LifeMax;
+			ExpGive *= LifesMax;
+			for (int i = 0; i < 40; ++i)
+			{
+				if (RealNPC.playerInteraction[i])
+				{
+					Starver.Players[i].UPGrade(ExpGive);
+				}
+			}
+			if (Drops != null)
+			{
+				DropItems();
+			}
+			KillMe();
+		}
+		#endregion
+		#region DropItems
+		protected void DropItems()
+		{
+			foreach (var item in Drops)
+			{
+				item.Drop(Main.npc[Index]);
+			}
+		}
+		#endregion
+		#region CheckTarget
+		public void CheckTarget()
+		{
+			if (Target < 0 || Target > 40 || TargetPlayer == null || !TargetPlayer.Active)
+			{
+				TargetClosest();
+				if (Target == -1)
+				{
+					if (AlivePlayers() < 0 && TShock.Utils.ActivePlayers() > 0)
+					{
+						OnFail();
+					}
+					else
+					{
+						KillMe();
+					}
+				}
+			}
+		}
+		#endregion
+		#region SummonFollows
+		protected unsafe void SummonFollows()
+		{
+			int alive = AlivePlayers();
+			int idx = NPC.NewNPC((int)Center.X, (int)Center.Y, (int)StarverAI[2], Index);
+			Main.npc[idx].life = Main.npc[idx].lifeMax = alive * 400;
+			Main.npc[idx].defense = 78;
+			Main.npc[idx].velocity = Rand.NextVector2(26.66f);
+			NetMessage.SendData((int)PacketTypes.NpcUpdate, -1, -1, null, idx);
+			idx = NPC.NewNPC((int)Center.X, (int)Center.Y, (int)StarverAI[3], Index);
+			Main.npc[idx].life = Main.npc[idx].lifeMax = alive * 400;
+			Main.npc[idx].defense = 78;
+			Main.npc[idx].velocity = Rand.NextVector2(26.66f);
+			NetMessage.SendData((int)PacketTypes.NpcUpdate, -1, -1, null, idx);
+		}
+		#endregion
+		#region OnFail
+		public virtual void OnFail()
+		{
+
+		}
+		#endregion
+		#region GoToTarget
+		public void GotoTarget()
+		{
+			Center = TargetPlayer.Center + NewByPolar(Rand.NextAngle(), 16 * 16);
+			SendData();
+		}
+		#endregion
+		#region ResetMode
+		protected void ResetMode()
+		{
+			lastMode = mode;
+			mode = BossMode.WaitForMode;
+		}
+		#endregion
+		#region Rush Begin
+		protected void RushBegin(bool NoRush = false)
+		{
+			modetime = 0;
+			lastMode = mode;
+			mode = BossMode.Rush;
+			if (!NoRush)
+			{
+				Center = TargetPlayer.Center + Rand.NextVector2(16 * 38f);
+				Rush();
+			}
+		}
+		#endregion
+		#region Rush
+		protected void Rush()
+		{
+			FakeVelocity = (Vector)(TargetPlayer.Center - Center) ;
+			FakeVelocity.Length = 26 * DamageIndex;
+		}
+		#endregion
+		#region Spawn
+		public unsafe virtual void Spawn(Vector2 where, int lvl = Criticallevel)
+		{
+			if (_active)
+			{
+				return;
+			}
+			_active = true;
+			Level = lvl;
+			ExVersion = Level > Criticallevel;
+			AliveBoss++;
+			Index = 0;
+			Index = NPC.NewNPC((int)where.X, (int)where.Y, RawType);
+			SendData();
+			Defense = DefaultDefense * (int)(100 * Math.Log(Level));
+			RealNPC.aiStyle = -1;
+			RealNPC.Center += Rand.NextVector2(54f, 54f);
+			RealNPC.noTileCollide = true;
+			//FakeVelocity = new Vector(ref RealNPC.velocity);
+			LifeMax = DefaultLife;
+			LifesMax = (int)(Level / (float)Criticallevel * DefaultLifes);
+			int count = TShock.Utils.ActivePlayers();
+			switch (LifeperPlayerType)
+			{
+				case ByLife:
+					LifeMax *= count;
+					break;
+				case ByLifes:
+					LifesMax *= count;
+					break;
+			}
+			Lifes = LifesMax;
+			LastCenter = Center;
+			Timer = 0;
+			if (ExVersion && FullName != null)
+			{
+				DisplayName = FullName;
+			}
+			else
+			{
+				DisplayName = Name;
+			}
+			if (!Silence)
+			{
+				TSPlayer.All.SendMessage(Language.GetTextValue("Announcement.HasAwoken", DisplayName), Color.MediumPurple);
+			}
+			modetime = 0;
+			LastCenter = Center;
+			for (float* begin = StarverAI, end = StarverAI + NumOfAIs; begin != end;)
+			{
+				*begin++ = 0;
+			}
+			mode = BossMode.WaitForMode;
+		}
+		#endregion
+		#region ToString
+		public override string ToString()
+		{
+			return string.Format("[Lv.{0}]{1}\nHP:{2}/{3}    Lifes:{4}/{5}\nMode:{6}", Level, Name, Life, LifeMax, Lifes, LifesMax, mode);
+		}
+		#endregion
+		#region lifes--
+		public virtual void LifeDown()
+		{
+			Lifes--;
+			if(Lifes < 1)
+			{
+				TSPlayer.All.SendMessage($"{DisplayName} 被打败了", Color.Blue);
+				BeDown();
+			}
+			else
+			{
+#if DEBUG
+				TSPlayer.All.SendMessage($"{Name}当前生命:{Lifes}/[c/0fff00:{LifesMax}]", Color.Yellow);
+#endif
+				if(Lifes % 10 == 0)
+				{
+					TSPlayer.All.SendMessage($"{DisplayName}被打掉了1条生命",Color.Blue);
+					TSPlayer.All.SendMessage($"{DisplayName}当前生命:{Lifes / 10}/[c/0fff00:{(int)(LifesMax / 10f + 0.999f)}]", Color.Blue);
+				}
+				LastCenter = Center;
+				RealNPC.life = RealNPC.lifeMax;
+				RealNPC.active = true;
+				SendData();
+			}
+		}
+		#endregion
+		#region checkactive
+		public virtual bool CheckActive()
+		{
+			if (!_active)
+			{
+				return _active;
+			}
+			else if (Index == -1)
+			{
+				return false;
+			}
+			else if (Lifes < 0)
+			{
+				KillMe();
+				return false;
+			}
+			return true;
+		}
+		#endregion
+		#region CheckDistance
+		protected bool CheckDistance()
+		{
+			if (Vector2.Distance(Center, TargetPlayer.Center) > MaxDistance)
+			{
+				if (IgnoreDistance)
+				{
+					GotoTarget();
+				}
+				else
+				{
+					KillMe();
+					return false;
+				}
+			}
+			return true;
+		}
+		#endregion
+		#region RealAI
+		public abstract void RealAI();
+		#endregion
+		#region AI
+		public unsafe override void AI(object args = null)
+		{
+			if (!CheckActive())
+			{
+				return;
+			}
+			if(!RealNPC.active)
+			{
+				RealNPC.active = true;
+				RealNPC.life = RealNPC.lifeMax;
+				RealNPC.Center = LastCenter;
+				SendData();
+			}
+			if (Target < 0 || Target >= 40 || TargetPlayer == null || !TargetPlayer.Active)
+			{
+				TargetClosest();
+				if (Target < 0 || Target >= 40 || TargetPlayer == null || !TargetPlayer.Active)
+				{
+					KillMe();
+					return;
+				}
+			}
+			if(Vector2.Distance(Center,TargetPlayer.Center) > 16 * 300)
+			{
+				if(IgnoreDistance)
+				{
+					Center = TargetPlayer.Center + Rand.NextVector2(16 * 20);
+				}
+				else
+				{
+					KillMe();
+					return;
+				}
+			}
+			if(NightBoss && Main.dayTime)
+			{
+				TSPlayer.Server.SetTime(false, 0);
+			}
+			++Timer;
+			RealAI();
+			if(WallActive)
+			{
+				UpdateWall();
+			}
+			++modetime;
+			#region Wasted
+			/*
+			if (Timer % 60 == 0)
+			{
+				Vector2 v = -RelativePos;
+				RealNPC.direction = (int)v.X;
+				RealNPC.directionY = (int)v.Y;
+			}
+			*/
+			#endregion
+			Velocity = FakeVelocity ;
+			SendData();
+		}
+		#endregion
+		#region StartWall
+		protected void StartWall(int target)
+		{
+			if (WallActive)
+			{
+				return;
+			}
+			WallActive = true;
+			if (Walls is null)
+			{
+				Walls = new int[MaxWalls];
+			}
+			if (WallVector is null)
+			{
+				WallVector = new Vector[MaxWalls];
+			}
+			WallTarget = Starver.Players[target];
+			for (int i = 0; i < MaxWalls; i++)
+			{
+				WallVector[i] = NewByPolar(PI * 4 * i / (3f * MaxWalls), 16 * 20);
+				Walls[i] = Proj(WallTarget.Center + WallVector[i], Vector2.Zero, ProjectileID.NebulaSphere, WallDamage, 0);
+			}
+			WallStop = false;
+			TimeSpan = 0;
+		}
+		#endregion
+		#region UpdateWall
+		private void UpdateWall()
+		{
+			if (WallTarget == null || !WallTarget.Active)
+			{
+				WallActive = false;
+			}
+			if (!WallStop)
+			{
+				if (TimeSpan > 60 * 20)
+				{
+					WallStop = true;
+					StopPos = (Vector)WallTarget.Center;
+				}
+			}
+			for (int i = 0; i < MaxWalls; i++)
+			{
+				if (Main.projectile[Walls[i]].active == false)
+				{
+					Walls[i] = Proj(WallTarget.Center + WallVector[i], Vector2.Zero, ProjectileID.NebulaSphere, WallDamage, 0);
+				}
+				if (!WallStop)
+				{
+					Main.projectile[Walls[i]].Center = WallVector[i] + WallTarget.Center;
+				}
+				else
+				{
+					Main.projectile[Walls[i]].Center = WallVector[i] + StopPos;
+				}
+				if (!WallStop)
+				{
+					WallVector[i].Angle += PI * 4 / 3 / MaxWalls;
+				}
+			}
+			TimeSpan++;
+			if (TimeSpan > 60 * 30)
+			{
+				WallActive = false;
+				for (int i = 0; i < MaxWalls; i++)
+				{
+					Main.projectile[Walls[i]].active = false;
+				}
+			}
+			for (int i = 0; i < MaxWalls; i++)
+			{
+				NetMessage.SendData((int)PacketTypes.ProjectileNew, Client, -1, null, Walls[i]);
+			}
+		}
+		#endregion
+		#region statics
+		public static bool EndTrial;
+		public static int AliveBoss { get; internal set; } = 0;
+		protected const byte ByLife = 0;
+		protected const byte ByLifes = 1;
+		#endregion
+	}
+}
