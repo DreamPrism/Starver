350bd154ee31a88aaf2b8403cc17bdc7694ded44
GUZZ07 <50436210+GUZZ07@users.noreply.github.com>
2019-07-14 18:35:21 +0800

    Create VortexBeaterEx.cs

---
+++ WeaponSystem/Weapons/Ranged/VortexBeaterEx.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starvers.WeaponSystem.Weapons.Ranged
+{
+	using IDs = Terraria.ID;
+	public class VortexBeaterEx : Weapon
+	{
+		#region ctor
+		public VortexBeaterEx() : base(0,IDs.ItemID.VortexBeater, IDs.ProjectileID.VortexBeaterRocket, CareerType.Ranged, 235)
+		{
+			CatchID = IDs.ProjectileID.VortexBeater;
+		}
+		#endregion
+	}
+}
