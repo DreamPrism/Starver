81a91bfeecc03388403e96c60d9b60bf7d8e3eca
GUZZ07 <50436210+GUZZ07@users.noreply.github.com>
2019-07-14 19:52:31 +0800

    Add files via upload

---
+++ NPCSystem/NPCs/BrainFollow.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using TShockAPI;
+
+namespace Starvers.NPCSystem.NPCs
+{
+	using Vector = TOFOUT.Terraria.Server.Vector2;
+	using BrainEx = BossSystem.Bosses.BrainEx;
+	public class BrainFollow : StarverNPC
+	{
+		#region Fields
+		private float Unit = PI / 100;
+		private int dmg;
+		private int Mod;
+		private BrainEx Brain;
+		private AIDelegate Rounding = null;
+		#endregion
+		#region Properties
+		public override float DamageIndex => Brain.DamageIndex;
+		#endregion
+		#region ctor
+		public BrainFollow():base(1)
+		{
+			RawType = NPCID.Creeper;
+			DefaultDefense = 2000;
+			DefaultLife = short.MaxValue;
+		}
+		#endregion
+		#region OnDead
+		public override void OnDead()
+		{
+			Brain.AliveFollows -= 1;
+			Starver.NPCs[Index] = null;
+		}
+		#endregion
+		#region Spawn
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="where"></param>
+		/// <param name="roundtype">0: 顺时针 1: 逆时针</param>
+		/// <returns></returns>
+		public bool Spawn(Vector where,BrainEx owner,float start,float Radium = 16 * 10,int roundtype = 0)
+		{
+			bool flag = base.Spawn(where);
+			Starver.NPCs[Index] = this;
+			RealNPC.aiStyle = None;
+			Brain = owner;
+			Target = Brain.Target;
+			Timer += (uint)(start * 10);
+			if(roundtype == 0)
+			{
+				dmg = 153;
+				Mod = 50;
+				Unit = PI / 100;
+				Rounding = Round;
+			}
+			else
+			{
+				dmg = 139;
+				Mod = 70;
+				Unit = PI / 50;
+				Rounding = Round_Negative;
+			}
+			Vel = NewByPolar(start, Radium);
+			//DamagedIndex = Criticallevel / (float)Brain.Level;
+			AIUsing[0] = Radium;
+			return flag;
+		}
+		#endregion
+		#region RealAI
+		protected override void RealAI()
+		{
+			if (!Brain.Active)
+			{
+				KillMe();
+				return;
+			}
+			Rounding();
+			Target = Brain.Target;
+			if (Timer % Mod  == 0)
+			{
+				FakeVelocity = (Vector)(TargetPlayer.Center - Center);
+				FakeVelocity.Length = 17.5f;
+				Proj(Center, FakeVelocity, ProjectileID.EyeLaser, dmg);
+				FakeVelocity = Vector.Zero;
+			}
+			//SendData();
+		}
+		#endregion
+		#region Round
+		/// <summary>
+		/// 顺时针
+		/// </summary>
+		private void Round()
+		{
+			Vel.Angle += Unit;
+			Center = Brain.Center + Vel;
+		}
+		/// <summary>
+		/// 逆时针
+		/// </summary>
+		private void Round_Negative()
+		{
+			Vel.Angle -= Unit;
+			Center = Brain.Center + Vel;
+		}
+		#endregion
+	}
+}
---
+++ NPCSystem/NPCs/PrimeExArm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+
+namespace Starvers.NPCSystem.NPCs
+{
+	using BossSystem.Bosses;
+	using Vector = TOFOUT.Terraria.Server.Vector2;
+	public class PrimeExArm : StarverNPC
+	{
+		#region Fields
+		private PrimeEx Prime;
+		private const float Distance = 16 * 25;
+		#endregion
+		#region ctor
+		public PrimeExArm() : base(1)
+		{
+			RawType = NPCID.PrimeLaser;
+			DefaultDefense = 2000;
+			DefaultLife = 30000;
+		}
+		#endregion
+		#region Properties
+		public override float DamageIndex => Prime.DamageIndex;
+		private BossMode Mode => Prime.ModeNow;
+		#endregion
+		#region Spawn
+		public unsafe void Spawn(Vector where,PrimeEx prime,float ID)
+		{
+			AIUsing[0] = PI * 2 * ID / PrimeEx.MaxArms;
+			Prime = prime;
+			base.Spawn(where);
+			Vel = NewByPolar(PI * 2 * (ID / PrimeEx.MaxArms),Distance);
+			Center = Prime.Center + Vel;
+			RealNPC.aiStyle = None;
+			RealNPC.ai[1] = Prime.Index;
+			SendData();
+		}
+		#endregion
+		#region RealAI
+		protected unsafe override void RealAI()
+		{
+			if(!Prime.Active)
+			{
+				KillMe();
+				return;
+			}
+			Target = Prime.Target;
+			AIUsing[0] += PI * 2 / PrimeEx.MaxArms / 20;
+			Vel = NewByPolar(AIUsing[0], Distance);
+			Center = Prime.Center + Vel;
+			switch(Mode)
+			{
+				#region Laser
+				case BossMode.PrimeExLaser:
+					if(Timer % 70 == 0)
+					{
+						Laser();
+					}
+					break;
+				#endregion
+				#region Rocket
+				case BossMode.PrimeExRocket:
+					if (Timer % 45 == 0)
+					{
+						Rocket();
+					}
+					break;
+				#endregion
+				#region Missile
+				case BossMode.PrimeExMissile:
+					if (Timer % 40 == 0)
+					{
+						Missile();
+					}
+					break;
+					#endregion
+			}
+			FakeVelocity = Vector.Zero;
+		}
+		#endregion
+		#region AIs
+		#region Missile
+		private void Missile()
+		{
+			Vel = (Vector)(Center - Prime.Center);
+			Vel.Length = 18;
+			Proj(Center, Vel, ProjectileID.SaucerMissile, 226);
+		}
+		#endregion
+		#region Rocket
+		private void Rocket()
+		{
+			Vel = (Vector)(TargetPlayer.Center - Prime.Center);
+			FakeVelocity = (Vector)(Center - Prime.Center);
+			if(Vel * FakeVelocity > 0)
+			{
+				Vel = (Vector)(TargetPlayer.Center - Center);
+				Vel.Length = 19;
+				Proj(Center, Vel, ProjectileID.RocketSkeleton, 195);
+			}
+			FakeVelocity = Vector.Zero;
+		}
+		#endregion
+		#region Laser
+		private void Laser()
+		{
+			Vel = (Vector)(TargetPlayer.Center - Prime.Center);
+			Vel.Length = 17;
+			Proj(Center, Vel, ProjectileID.SaucerLaser, 193);
+		}
+		#endregion
+		#endregion
+	}
+}
