04537a77f1cf9c79783491a6209dc63d547efada
GUZZ07 <50436210+GUZZ07@users.noreply.github.com>
2019-07-14 18:37:50 +0800

    Add files via upload

---
+++ WeaponSystem/StarverWeaponManager.cs
@@ -0,0 +1,90 @@
+﻿using Starvers.WeaponSystem.Weapons;
+using Starvers.WeaponSystem.Weapons.Ranged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starvers.WeaponSystem
+{
+	using Vector = TOFOUT.Terraria.Server.Vector2;
+	public class StarverWeaponManager : IStarverPlugin
+	{
+		#region interfaces
+		public StarverConfig Config => StarverConfig.Config;
+		public bool Enabled => Config.EnableAura && Config.EnableBoss;
+		public void Load()
+		{
+			TShockAPI.GetDataHandlers.NewProjectile += OnProj;
+		}
+		public void UnLoad()
+		{
+			TShockAPI.GetDataHandlers.NewProjectile -= OnProj;
+		}
+		#endregion
+		#region Hooks
+		private void OnProj(object sender,TShockAPI.GetDataHandlers.NewProjectileEventArgs args)
+		{
+			if(args.Owner > 40 || Terraria.Main.projectile[args.Index].hostile)
+			{
+				return;
+			}
+			Weapon[] weapons;
+			if(Terraria.Main.projectile[args.Index].melee)
+			{
+				weapons = WeaponList[CareerType.Melee];
+			}
+			else if (Terraria.Main.projectile[args.Index].ranged)
+			{
+				weapons = WeaponList[CareerType.Ranged];
+			}
+			else if (Terraria.Main.projectile[args.Index].magic)
+			{
+				weapons = WeaponList[CareerType.Magic];
+			}
+			else
+			{
+				weapons = WeaponList[CareerType.Minion];
+			}
+			foreach(var weapon in weapons)
+			{
+				if(weapon.IsThis(args.Index))
+				{
+					if (Starver.Players[args.Owner].HasWeapon(weapon))
+					{
+						weapon.UseWeapon(Starver.Players[args.Owner], (Vector)args.Velocity);
+					}
+					break;
+				}
+			}
+		}
+		private delegate void WeaponDelegate(StarverPlayer player,Vector Velocity);
+		#endregion
+		#region Fields
+		private static Weapon[] Melee =
+		{
+
+		};
+		private static Weapon[] Ranged =
+		{
+			new VortexBeaterEx()
+		};
+		private static Weapon[] Magic =
+		{
+			
+		};
+		private static Weapon[] Minion =
+		{
+
+		};
+		private static Weapon[][] WeaponList =
+		{
+			Melee,
+			Ranged,
+			Magic,
+			Minion
+		};
+		#endregion
+	}
+}
